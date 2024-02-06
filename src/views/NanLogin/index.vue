c38d1ebb4027ec63a271cf7b0565dab3c75ed000
chh <@chh.com>
2024-02-06 21:39:07 +0800

    登入start

--- src/views/NanLogin/index.vue
+++ src/views/NanLogin/index.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="login-container">
-    <div class="login-container_left" />
-    <div class="login-container_right">
-      <el-card shadow="never">
-        <el-form ref="form" :rules="rules" :model="form">
-          <el-form-item prop="userCount">
-            <el-input v-model="form.userCount"  placeholder="请输入学号或职工编号" />
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input show-password v-model="form.password" placeholder="请输入密码" />
-          </el-form-item>
-          <el-form-item prop="isAgree">
-            <el-checkbox v-model="form.isAgree">平台使用协议</el-checkbox>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click.native="login">登录</el-button>
-          </el-form-item>
-        </el-form>
-      </el-card>
+    <div class="login-box">
+      <div class="login-container_left">
+        <ul class="user-info">
+          <li class="student">学生登入</li>
+          <li class="teacher">教师登入</li>
+          <li class="instructor">辅导员登入</li>
+          <li class="administrator">管理员登入</li>1111
+        </ul>
+      </div>
+      <div class="login-container_right">
+        <h1 class="user-name">学生登录</h1>
+        <el-card shadow="never">
+          <el-form ref="form" :rules="rules" :model="form">
+            <el-form-item prop="userCount" class="user-Count">
+              <el-input v-model="form.userCount"  placeholder="请输入学号或职工编号" />
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input show-password v-model="form.password" placeholder="请输入密码" />
+            </el-form-item>
+            <el-form-item prop="isAgree">
+              <el-checkbox v-model="form.isAgree">平台使用协议</el-checkbox>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click.native="login">登录</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+      </div>
     </div>
   </div>
 </template>
@@ -72,25 +82,49 @@ export default {
 .login-container {
   display: flex;
   height: 100vh;
-  .login-container_left {
-    flex: 3;
-    background: {
-        image: url(https://pica.zhimg.com/80/a52a2e42c77c767b82aaf4b0f4b1dd52_720w.webp);
-        repeat: no-repeat;
-        position: center;
-        size: cover;
+  align-items: center;
+  justify-content: center;
+  .login-box {
+    position: relative;
+    height: 700px;
+    width: 1200px;
+    border: 1px solid red;
+    .login-container_left {
+      .user-info {
+        display: flex;
+        justify-content: space-around;
+        width: 500px;
+        border: 1px solid paleturquoise;
+
+        li {
+          font-weight: 600;
+          color: #7d7878;
+          padding: 15px 15px;
+          letter-spacing: 2px;
+          cursor: pointer;
+        }
+        li:hover {
+          color: rgb(133, 229, 234);
+        }
+      }
     }
-    border-top-right-radius: 60px;
-  }
+  //   .login-container_left {
+  // }
   .login-container_right {
-    flex: 2;
+    position: absolute;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    padding-left: 170px;
-    h1 {
-      padding-left: 5px;
-      font-size: 24px;
+    align-items: center;
+    height: 550px;
+    width: 480px;
+    border: 1px solid skyblue;
+    right: 100px;
+    top: 50%;
+    transform: translateY(-50%);
+    .user-name {
+      font-size: 25px;
+      font-weight: 600;
+      padding:  50px;
     }
     .el-card {
       border: none;
@@ -99,9 +133,13 @@ export default {
       }
       .el-input {
       width: 350px;
+      border-left: none;
         ::v-deep .el-input__inner {
           background: #f4f5fb;
         }
+      }
+      input.el-input__inner {
+        border-left: none;
       }
       .el-checkbox {
         color:#606266;
@@ -109,7 +147,11 @@ export default {
       .el-button {
         width:350px
       }
+      .user-Count {
+        border: none;
+      }
     }
+  }
   }
 }
 </style>
