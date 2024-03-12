50f6230a319e18e6dcd8efac9999b88ec9bd6769
ZRMYDYCG <547471919@qq.com>
2024-03-12 15:36:43 +0800

    封装 Nan-tag 组件 用于面包屑导航

---
+++ src/Layout/Bread/index.vue
@@ -0,0 +1,30 @@
+<script>
+import simplebar from 'simplebar-vue'
+import 'simplebar-vue/dist/simplebar.min.css'
+export default {
+  name: 'LayoutBread',
+  data () {},
+  components: {
+    simplebar
+  }
+}
+</script>
+
+<template>
+  <div>
+    <simplebar data-simplebar-auto-hide="false">
+      // your content elements
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </simplebar>
+  </div>
+</template>
+
+<style scoped>
+::v-deep .simplebar-scrollbar::before {
+  background-color: red;
+}
+</style>
