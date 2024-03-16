da5fb906d02c73e9b8cf9bbbc7498feeb47a3e09
ZRMYDYCG <547471919@qq.com>
2024-03-16 21:57:27 +0800

    解决左侧菜单导航在路由变化时无法自动激活的 bug

--- src/Layout/Menu/index.vue
+++ src/Layout/Menu/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="LayoutMenu-container">
-    <el-menu class="el-menu-vertical-demo" :collapse="$store.state.setting.isCollapse">
-      <el-menu-item v-for="(item, index) in menuList" :key="item.path" :index="1 + index.toString()" @click.native="CheckViews(item)">
+    <el-menu class="el-menu-vertical-demo"
+             :collapse="$store.state.setting.isCollapse"
+             :default-active="activeIndex">
+      <el-menu-item v-for="(item, index) in menuList"
+                    :key="index"
+                    :index="item.path"
+                    @click.native="CheckViews(item)">
         <img :src="item.meta.icon.url" class="el-menu-item-img_menuIcon">
-        <span class="el-menu-item-span_title" slot="title">{{item.meta.title}}</span>
+        <span class="el-menu-item-span_title" slot="title">
+          {{item.meta.title}}</span>
       </el-menu-item>
     </el-menu>
   </div>
 </template>
-
 <script>
 import { AsideRoutes } from '@/router'
 export default {
   name: 'LayoutMenu',
   data () {
     return {
-      menuList: AsideRoutes[0].children
+      menuList: AsideRoutes[0].children,
+      activeIndex: ''
     }
   },
   methods: {
@@ -28,17 +34,23 @@ export default {
       this.$store.dispatch('tags/add_TagItem', TargetObj)
     }
   },
+  watch: {
+    $route (to, from) {
+      this.activeIndex = to.path
+    }
+  },
   mounted () {
-    console.log(this.menuList)
+    this.activeIndex = this.$route.path
   }
 }
 </script>
-
 <style lang="scss" scoped>
 .LayoutMenu-container {
   margin-top: 10px;
+
   .el-menu {
     border-right: none;
+
     .el-menu-item {
       .el-menu-item-img_menuIcon {
         width: 20px;
