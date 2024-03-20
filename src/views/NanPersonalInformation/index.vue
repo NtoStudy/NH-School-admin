9c00e11f1b8d0c2aa5a0d50ce8f806c8c00d0ad0
ZRMYDYCG <547471919@qq.com>
2024-03-20 23:49:44 +0800

    个人信息提交页左右两栏架子搭好

--- src/views/NanPersonalInformation/index.vue
+++ src/views/NanPersonalInformation/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="NanPersonalInformation"></div>
+  <div class="NanPersonalInformation">
+    <el-row :gutter="20">
+      <el-col class="left" style="padding-left: 0" :span="4">
+        <div class="left_menu"></div>
+      </el-col>
+      <el-col class="right" style="padding-right: 0" :span="20">
+        <div class="right_scrollView"></div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -11,4 +20,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+  height: 80vh;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: #dce0eb 0 0 10px 0;
+  .el-col {
+    height: 100%;
+  }
+  .left {
+    background-color: #dce0eb;
+    .left_menu {
+      height: 100%;
+      background-color: #fff;
+    }
+  }
+  .right {
+    background-color: #dce0eb;
+    .right_scrollView {
+      height: 100%;
+      background-color: #fff;
+    }
+  }
+}
+</style>
